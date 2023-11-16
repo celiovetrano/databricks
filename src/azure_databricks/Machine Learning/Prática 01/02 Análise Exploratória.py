96f7359a575e24641086091176259d30a09f004a
celiovetrano <149624872+celiovetrano@users.noreply.github.com>
2023-11-16 14:29:10 -0300

    Add files via upload

--- src/azure_databricks/Machine Learning/Prática 01/02 Análise Exploratória.py
+++ src/azure_databricks/Machine Learning/Prática 01/02 Análise Exploratória.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 # COMMAND ----------
 
-repository_path = "file:/Workspace/Repos/celio.vetrano@gmail.com/databricks/"
+repository_path = "file:/Workspace/Repos/emanuelfontelles@hotmail.com/Mastercloud-Trilha-03-Onda-2/"
 data_path = repository_path + "src/azure_databricks/data/"
 
 # COMMAND ----------
@@ -73,6 +73,14 @@ plt.show()
 
 # COMMAND ----------
 
+df['inadimplente'].value_counts()
+
+# COMMAND ----------
+
+df["salario_mensal"].describe()
+
+# COMMAND ----------
+
 import matplotlib.pyplot as plt
 
 df.hist(bins=50, figsize=(20,15))
@@ -94,27 +102,41 @@ plt.show()
 
 # COMMAND ----------
 
-df['salario_mensal'] = df['salario_mensal'].apply(np.log)
-
-# COMMAND ----------
-
-sns.boxplot(x='inadimplente', y='salario_mensal', data=df)
-plt.title('Boxplot de Idade por Inadimplência')
-plt.show()
-
-# COMMAND ----------
-
-sns.boxplot(x='inadimplente', y='salario_mensal', data=df_filtrado)
-plt.title('Boxplot de Idade por Inadimplência')
-plt.show()
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC #### Scatter Plots (Nuvem de pontos)
 # MAGIC Conceito:
 # MAGIC  - Scatter plots são utilizados para visualizar a relação entre duas variáveis quantitativas.
 # MAGIC  - Úteis para identificar padrões de correlação.
+
+# COMMAND ----------
+
+# Calculando Q1 e Q3
+Q1 = df['salario_mensal'].quantile(0.25)
+Q3 = df['salario_mensal'].quantile(0.75)
+IQR = Q3 - Q1
+ 
+# Definindo limites para outliers
+limite_inferior = Q1 - 1.5 * IQR
+limite_superior = Q3 + 1.5 * IQR
+ 
+# Removendo os outliers
+df_filtrado = df[(df['salario_mensal'] >= limite_inferior) & (df['salario_mensal'] <= limite_superior)]
+
+# COMMAND ----------
+
+sns.scatterplot(data=df_filtrado, x="idade", y="salario_mensal", hue='inadimplente', alpha=0.5)
+
+# COMMAND ----------
+
+sns.scatterplot(data=df, x="idade", y="numero_emprestimos_imobiliarios", hue='inadimplente', alpha=0.2)
+
+# COMMAND ----------
+
+sns.scatterplot(data=df_filtrado, x="salario_mensal", y="numero_emprestimos_imobiliarios", hue='inadimplente', alpha=0.2)
+
+# COMMAND ----------
+
+sns.scatterplot(data=df_filtrado, x="numero_de_dependentes", y="salario_mensal", hue='inadimplente', alpha=0.2)
 
 # COMMAND ----------
 
@@ -137,10 +159,6 @@ limite_superior = Q3 + 1.5 * IQR
  
 # Removendo os outliers
 df_filtrado = df[(df['salario_mensal'] >= limite_inferior) & (df['salario_mensal'] <= limite_superior)]
- 
-# Visualizando o resultado
-print("Original:", df.shape)
-print("Após remoção de outliers:", df_filtrado.shape)
 
 # COMMAND ----------
 
@@ -152,11 +170,56 @@ plt.show()
 
 # COMMAND ----------
 
-sns.pairplot(df_filtrado)
+# MAGIC %md
+# MAGIC #### Histograma (KDE) para inadimplência
 
 # COMMAND ----------
 
-df_filtrado.corr()
+plt.figure(figsize = (10, 8))
+
+# KDE plot of loans that were repaid on time
+sns.kdeplot(df.loc[df['inadimplente'] == 0, 'idade'], label = 'Bom pagador')
+
+# KDE plot of loans which were not repaid on time
+sns.kdeplot(df.loc[df['inadimplente'] == 1, 'idade'], label = 'Mau pagador')
+
+# Labeling of plot
+plt.xlabel('Idade (anos)'); 
+plt.ylabel('Density'); 
+plt.title('Distribuição das idades');
+plt.legend();
+
+# COMMAND ----------
+
+plt.figure(figsize = (10, 8))
+
+# KDE plot of loans that were repaid on time
+sns.kdeplot(df.loc[df['inadimplente'] == 0, 'numero_emprestimos_imobiliarios'], label = 'Bom pagador')
+
+# KDE plot of loans which were not repaid on time
+sns.kdeplot(df.loc[df['inadimplente'] == 1, 'numero_emprestimos_imobiliarios'], label = 'Mau pagador')
+
+# Labeling of plot
+plt.xlabel('numero_emprestimos_imobiliarios (anos)'); 
+plt.ylabel('Density'); 
+plt.title('Distribuição das numero_emprestimos_imobiliarios');
+plt.legend();
+
+# COMMAND ----------
+
+plt.figure(figsize = (10, 8))
+
+# KDE plot of loans that were repaid on time
+sns.kdeplot(df.loc[df['inadimplente'] == 0, 'numero_linhas_crdto_aberto'], label = 'Bom pagador')
+
+# KDE plot of loans which were not repaid on time
+sns.kdeplot(df.loc[df['inadimplente'] == 1, 'numero_linhas_crdto_aberto'], label = 'Mau pagador')
+
+# Labeling of plot
+plt.xlabel('numero_linhas_crdto_aberto (anos)'); 
+plt.ylabel('Density'); 
+plt.title('Distribuição das idades');
+plt.legend();
 
 # COMMAND ----------
 
@@ -165,6 +228,35 @@ df_filtrado.corr()
 # MAGIC Conceito:
 # MAGIC  - A correlação mede a relação estatística entre duas variáveis.
 # MAGIC  - O coeficiente de correlação varia entre -1 e 1. Valores próximos a 1 indicam forte correlação positiva, enquanto valores próximos a -1 indicam forte correlação negativa.
+
+# COMMAND ----------
+
+df['idade_cat'] = pd.cut(df['idade'], bins=list(range(18, 81, 5)), right=False)
+
+# COMMAND ----------
+
+df['idade_cat'].value_counts(normalize=True).sort_index().plot.bar()
+
+# COMMAND ----------
+
+df.groupby('idade_cat')['inadimplente'].mean().plot()
+
+# COMMAND ----------
+
+df[['inadimplente','idade', 'salario_mensal', 'numero_linhas_crdto_aberto', 'numero_emprestimos_imobiliarios']].corr()
+
+# COMMAND ----------
+
+df_filtrado[['inadimplente','idade', 'salario_mensal', 'numero_linhas_crdto_aberto', 'numero_emprestimos_imobiliarios']].corr('spearman')
+
+# COMMAND ----------
+
+df.corr()
+
+# COMMAND ----------
+
+ax, fig = plt.subplots(figsize=(16, 16))
+sns.heatmap(df_filtrado.drop(['inadimplente'], axis=1).corr(), annot=True, fmt=".2f")
 
 # COMMAND ----------
 
@@ -193,6 +285,10 @@ df.head()
 
 # COMMAND ----------
 
+df['inadimplente']
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ### Após a construção de novas informações salvamos o novo dataset na nossa camada `gold`
 
@@ -203,7 +299,3 @@ df_silver = spark.createDataFrame(df)
 # COMMAND ----------
 
 df_silver.write.parquet(data_path+"gold/credito_inadimplencia", mode="overwrite")
-
-# COMMAND ----------
-
-df.corr()
